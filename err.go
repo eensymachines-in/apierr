2905acfa843c192f2e947879939af17664493ad9
Niranjan Awati <kneerunjun@gmail.com>
2020-10-09 19:00:03 +0530

    added chain functions for setting properties

--- err.go
+++ err.go
@@ -10,28 +10,64 @@ import (
 
 // APIErr : ineternal api error that has the http status code too
 type APIErr struct {
-	ErrCode int    // this is the error code that would translate to http error status
-	Msg     string // this is the error message
-	Context string // this is the location where the error has ocured
+	code    int    // this is the error code that would translate to http error status
+	msg     string // this is the error message
+	ctx     string // this is the location where the error has ocured
+	loginfo string // this shall be the deep information of why the error has occured
 }
 
 // APIErrrFrom : creates a new instance of the error from other lib error
-func APIErrrFrom(e error, context string) *APIErr {
+func APIErrrFrom(e error, context, log string) *APIErr {
 	// This will default to internal error
-	return &APIErr{500, e.Error(), context}
+	return &APIErr{500, e.Error(), context, log}
 }
 
 // NewAPIErrr : creates a new error instance of the APIErr
-func NewAPIErrr(code int, msg, context string) *APIErr {
-	return &APIErr{code, msg, context}
+func NewAPIErrr(code int) *APIErr {
+	result := &APIErr{}
+	result.code = code
+	return result
 }
 func (apie *APIErr) Error() string {
-	return apie.Msg
+	return apie.msg
 }
+
+// --------------------------
+// Below are the chained functions that help set the various props
+// --------------------------
+
+// Code : helps to set the code of the Error object
+// code is the HTTP code that is used over the response
+func (apie *APIErr) Code(code int) *APIErr {
+	apie.code = code
+	return apie
+}
+
+// Message : sets the message of the error
+func (apie *APIErr) Message(m string) *APIErr {
+	apie.msg = m
+	return apie
+}
+
+// Cotext : sets the context of the error - generally this is where the error has emanated from
+func (apie *APIErr) Cotext(c string) *APIErr {
+	apie.ctx = c
+	return apie
+}
+
+// LogInfo : sets the logging info and can be used in a chain
+func (apie *APIErr) LogInfo(li string) *APIErr {
+	apie.loginfo = li
+	return apie
+}
+
+// ------------------------
+// this helps in the client side code to get error details
+// ------------------------
 
 // ToHTTPContext : this converts the error to http context
 func (apie *APIErr) ToHTTPContext(c *gin.Context) {
-	c.AbortWithStatusJSON(apie.ErrCode, gin.H{
+	c.AbortWithStatusJSON(apie.code, gin.H{
 		"err": apie.Error(),
 	})
 	return
@@ -39,5 +75,5 @@ func (apie *APIErr) ToHTTPContext(c *gin.Context) {
 
 // Log : this persists the error to the log as configured
 func (apie *APIErr) Log() {
-	log.Errorf("%s: %d: %s", apie.Context, apie.ErrCode, apie.Error())
+	log.Errorf("%s: %d: %s", apie.ctx, apie.code, apie.Error())
 }
